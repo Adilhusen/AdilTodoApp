52b269c7182d7ca3ea89843bdb0114d84c5db4d3
Adil <adilb@zignuts.com>
2022-04-16 12:48:53 +0530

    Implementing UI

--- app/src/main/java/com/ad/adilpracticaltask/presentation/MainActivity.kt
+++ app/src/main/java/com/ad/adilpracticaltask/presentation/MainActivity.kt
@@ -3,10 +3,14 @@ package com.ad.adilpracticaltask.presentation
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.ad.adilpracticaltask.R
+import com.ad.adilpracticaltask.base.BaseActivity
+import com.ad.adilpracticaltask.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
